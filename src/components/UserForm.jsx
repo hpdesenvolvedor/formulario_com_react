0eab12761a67e450d22c25e8206ff54610b96887
Henrique <nuneshp2@gmail.com>
2023-10-12 19:25:46 -0300

    formulário com react

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UserForm = ({data, updateFieldHandler}) => {
+  return (
+    <div className="form-control">
+      <label htmlFor="name">Nome:</label>
+      <input
+        type="text"
+        name="text"
+        id="name"
+        placeholder="Digite seu nome"
+        required
+        value={data.name || ""}
+        onChange={(e) => updateFieldHandler("name", e.target.value)}
+      />
+      <div className="form-control">
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+          required
+          value={data.email || ""}
+        onChange={(e) => updateFieldHandler("email", e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserForm;
